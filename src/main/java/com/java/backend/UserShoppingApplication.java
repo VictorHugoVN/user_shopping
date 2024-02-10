69151c5c7d1b365b32c55e4f5aecd596e0fc7394
Victor Hugo <victorneivadef8@gmail.com>
2024-02-10 17:42:50 -0300

    Projeto criado

---
+++ src/main/java/com/java/backend/UserShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.java.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserShoppingApplication.class, args);
+	}
+
+}
